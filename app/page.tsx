befc75c2b490fd74cf946f7c7cdd76632ad96594
Marvin <marvinraj77@gmail.com>
2025-04-03 00:33:10 +0800

    fix spelling mistake

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>this would me the main page, obviously.</h1>
+    <h1>this would be the main page, obviously.</h1>
   );
 }
